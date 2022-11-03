800137132576997643dd531d74a3abb3cfbfa0e3
apenasliana <miresukali@gmail.com>
2022-11-03 14:54:26 -0300

    day 1 complete, configuring  web-mobile-server

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,8 +1,26 @@
 
-// 55 min
-export default function Home() {
+interface HomeProps {
+  count: number;
+}
+
+
+export default function Home(props: HomeProps) {
   return (
     <h1>hello world</h1>
   
   )
+}
+
+export const getServerSideProps =async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  return{
+    props: {
+      count:{
+        count: data.count,
+      }
+    }
+  }
+  
 }
